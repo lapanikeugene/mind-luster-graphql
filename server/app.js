3a8a19b719bafd7659e57576b8c89d4e053dc0c7
lapanikeugene <eugeny.lp@gmail.com>
2023-03-17 11:50:27 +0100

    tutorial 6 done

--- server/app.js
+++ server/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
-const graphQL = require('express-graphql');
+// handle graphql request.
+const graphQLHTTP = require('express-graphql').graphqlHTTP;
 
 const app = express();
+
+app.use('/graphql',graphQLHTTP({
+
+}))
+
+
 app.listen(4000,()=>{
     console.log("listening port ", 4000);
 });

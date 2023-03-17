96a84fb786374430d7225f7abc5cd29dd3a9a95e
lapanikeugene <eugeny.lp@gmail.com>
2023-03-17 12:42:04 +0100

    8th tutorial completed.

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 // handle graphql request.
 const graphQLHTTP = require('express-graphql').graphqlHTTP;
-
+const schema = require("./schema/schema");
 const app = express();
 
 app.use('/graphql',graphQLHTTP({
-
+schema
 }))
 
 
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,7 +3,7 @@ const graphQL = require('graphql');
 
 // GraphQLString - special string type for graphql
 // GraphQLObjectType - create schemas. 
-const {GraphQLObjectType,GraphQLString} =graphQL;
+const {GraphQLObjectType,GraphQLString,GraphQLSchema} =graphQL;
 
 const BookType = new GraphQLObjectType({
     name:'Book',
@@ -13,3 +13,28 @@ const BookType = new GraphQLObjectType({
         genre:{type:GraphQLString},
     })
 });
+
+/**
+ * The Root Query is essentially a GraphQL object type, and it is the first type that is executed
+ *  when a GraphQL query is made. It contains a set of fields that correspond to 
+ * the available data that can be queried by the client.
+ * fields without object - we don't need to save the order of fields. 
+ */
+const RootsQuery = new GraphQLObjectType({
+    name:'RootQueryType',
+    fields:{
+        book:{
+            type:BookType,
+            //what book does user need? Ask him about id of book.  
+            args:{id:{type:GraphQLString}},
+            resolve(parent,args){
+                // code to get data from db. 
+
+            }
+        }
+    }
+})
+
+
+module.exports = new GraphQLSchema({
+    query:RootsQuery});

3db1556d4c270b77c246a074c5290c157bdfa680
lapanikeugene <eugeny.lp@gmail.com>
2023-03-18 14:59:37 +0100

    19 tutorial done.

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -123,6 +123,25 @@ const mutations = new GraphQLObjectType({
                 //save to database and return result. 
                 return author.save();
             }
+        },
+        addBook:{
+            type:BookType,
+            args:{
+                name:       {type:GraphQLString},
+                genre:      {type:GraphQLString},
+                authorId:   {type:GraphQLID},
+
+
+            },
+            resolve(parent,args){
+                let book = new Book({
+                    name: args.name,
+                    genre: args.genre, 
+                    authorid: args.authorId,
+                });
+
+                return book.save();
+            }
         }
     }
 })

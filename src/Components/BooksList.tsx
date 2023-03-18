e911d25594f0940f6afb7a5ba0b80af47d652c89
lapanikeugene <eugeny.lp@gmail.com>
2023-03-18 23:01:19 +0100

    course complete

--- src/Components/BooksList.tsx
+++ src/Components/BooksList.tsx
@@ -1,23 +1,32 @@
 import { useQuery } from "@apollo/client";
 import { gql } from "@apollo/client/core"
 import { graphql } from "graphql"
+import { useState } from "react";
 import useGetQuery from "../assets/GetQuery";
 import { getBookQuery } from "../Queries/queries";
+import BookDetails from "./BookDetails";
 
 
 interface books{
     name: string, 
     id: string,
 }
+
+
 const BookList = ()=>{
     const bookData = useGetQuery(getBookQuery);
-   
+    const [bookId,setBookId] = useState("");
 
+    const handleClick=(id:string)=>(e:React.MouseEvent)=>{
+        console.log("click on li")
+        setBookId(id);
+    
+    }
     return (<>
     <ul id="book-list">
       {bookData ? <>{(bookData.books as unknown  as books[])?.map((a,i)=>{
         return<>
-        <li key={a.id}>
+        <li key={a.id} onClick={handleClick(a.id)}>
             {a.name}
         </li>
         </>
@@ -26,6 +35,10 @@ const BookList = ()=>{
       Loading...
       </>}
     </ul>
+
+    <BookDetails bookId={bookId} key={bookId} />
+
+
     </>)
 }
 

aa94f1b7d2736ec05ad383b24ca9bacd5af22f50
lapanikeugene <eugeny.lp@gmail.com>
2023-03-18 17:33:40 +0100

    24 done

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import BookList from './Components/BooksList'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <h1>GraphQL Course' tutorial</h1>
+      <BookList />
     </div>
   )
 }
---
+++ src/Components/BooksList.tsx
@@ -0,0 +1,16 @@
+
+const BookList = ()=>{
+
+
+
+    return (<>
+    <ul id="book-list">
+        <li>
+            Book Name
+        </li>
+    </ul>
+    </>)
+}
+
+
+export default BookList

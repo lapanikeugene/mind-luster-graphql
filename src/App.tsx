9379bc4be1c042adf501dd24929036a8b0f20701
lapanikeugene <eugeny.lp@gmail.com>
2023-03-18 17:47:42 +0100

    25 done

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import BookList from './Components/BooksList'
+import { ApolloClient } from '@apollo/client'
+import { InMemoryCache } from '@apollo/client/cache'
+import { ApolloProvider } from '@apollo/client/react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const appoloClient  = new ApolloClient({
+    uri:"http://localhost:4000/graphql",
+    cache: new InMemoryCache()
+  })
 
   return (
     <div className="App">
       <h1>GraphQL Course' tutorial</h1>
-      <BookList />
+      <ApolloProvider client={appoloClient} >
+        <BookList />
+      </ApolloProvider>
     </div>
   )
 }
